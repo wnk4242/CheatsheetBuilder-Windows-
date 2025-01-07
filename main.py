99275c95549b49cd07abb4fe1198c7ce52876403
Naike <wangnaike@tamu.edu>
2025-01-07 12:49:28 -0600

    Enlarged window hight

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from menu import main_menu
 import os
 
 # Set Command Prompt dimensions
-os.system("mode con: cols=110 lines=25")
+os.system("mode con: cols=110 lines=30")
 
 if __name__ == "__main__":
     # Start the main menu
